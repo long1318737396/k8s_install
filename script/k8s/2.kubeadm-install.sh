490dd4cbde1015f5ac4029678d0c162322e1782d
long1318737396 <long1318737396@163.com>
2024-02-23 23:42:58 +0800

    update

--- script/k8s/2.kubeadm-install.sh
+++ script/k8s/2.kubeadm-install.sh
@@ -12,12 +12,14 @@ tar -C $kubeadm_dir -xzf crictl-${crictl_version}-linux-${arch}.tar.gz
 
 #RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
 
-/bin/cp  {kubeadm,kubelet,kubectl} $kubeadm_dir/
+tar -zxvf kubernetes-server-linux-${arch}.tar.gz
+/bin/cp kubernetes/server/bin/{kubectl,kubeadm} $kubeadm_dir/
+/bin/cp  kubeadm $kubeadm_dir/
 chmod +x $kubeadm_dir/{kubeadm,kubelet,kubectl}
 
-cat ../../conf/kubelet.service | sed "s:/usr/bin:${kubeadm_dir}:g" |tee /etc/systemd/system/kubelet.service
+cat ../../../conf/kubelet.service | sed "s:/usr/bin:${kubeadm_dir}:g" |tee /etc/systemd/system/kubelet.service
 mkdir -p /etc/systemd/system/kubelet.service.d
-cat ../../conf/10-kubeadm.conf | sed "s:/usr/bin:${kubeadm_dir}:g" | tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+cat ../../../conf/10-kubeadm.conf | sed "s:/usr/bin:${kubeadm_dir}:g" | tee /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 systemctl enable --now kubelet
 echo "source <(kubectl completion bash)" > /etc/profile.d/kubectl.sh
@@ -27,5 +29,5 @@ echo "source <(kubeadm completion bash)" > /etc/profile.d/kubeadm.sh
 mkdir -p /etc/kubernetes/manifests/
 if [[ "$kube_vip_enable" == "true"]]
   then
-    cat ../../yaml/first-master-kube-vip.yaml |sed -e "s/\${kube_vip}/${kube_vip}/g" -e "s/\{kube_vip_eth}/${kube_vip_eth}/g" |tee /etc/kubernetes/manifests/kube-vip.yaml
+    cat ../../../yaml/first-master-kube-vip.yaml |sed -e "s/\${kube_vip}/${kube_vip}/g" -e "s/\{kube_vip_eth}/${kube_vip_eth}/g" |tee /etc/kubernetes/manifests/kube-vip.yaml
 fi
