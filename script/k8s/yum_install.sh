3aad1695e7d0c838efcc3d0f32276791d02a76ae
long1318737396 <long1318737396@163.com>
2024-02-23 18:39:00 +0800

    update

--- script/k8s/yum_install.sh
+++ script/k8s/yum_install.sh
@@ -7,4 +7,5 @@ exec > >(tee -a "$logfile") 2>&1
 echo "$date_format"
 
 cd ../../offline/yum/${arch}
-yum localinstall wget conntrack socat ipvsadm ipset  telnet bind-utils nmap nfs-utils  bash-completion tcpdump mtr nftables iproute-tc -y
+
+yum localinstall wget* conntrack* socat* ipvsadm* ipset*  telnet* bind-utils*  nfs-utils* unzip* bash-completion* tcpdump* mtr* nftables* iproute-tc*  --skip-broken -y
