c7e4c0b1d496a33a662482b13283e8e108b246c7
long1318737396 <long1318737396@163.com>
2024-02-27 12:38:03 +0800

    update

--- 6.k8s_install.sh
+++ 6.k8s_install.sh
@@ -7,4 +7,8 @@ echo "$date_format"
 
 bash script/k8s/2.kubeadm-install.sh
 bash script/k8s/3.k8s-install.sh
+if [ $? -ne 0 ]; then
+  echo "Command failed. Exiting..."
+  exit 1
+fi
 bash script/k8s/4.net-work.sh
