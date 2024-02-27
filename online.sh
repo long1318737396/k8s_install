1e68a76e053c1c2f05af23b6ab19195a1c334248
long1318737396 <long1318737396@163.com>
2024-02-27 13:07:11 +0800

    update

--- online.sh
+++ online.sh
@@ -1,6 +1,7 @@
 apt install conntrack socat ipvsadm ipset git telnet dnsutils nfs-kernel-server nfs-common bash-completion iptables wget -y
 yum install conntrack socat ipvsadm ipset git telnet dns-utils nfs-utils bash-completion  wget -y
 bash offline/bin/amd64/download-binary-online.sh
+hostnamectl set-hostname master1
 bash 2.init.sh
 bash 3.docker_install.sh
 k8s_version=v1.29.2
@@ -9,6 +10,6 @@ wget --no-check-certificate https://dl.k8s.io/release/${k8s_version}/bin/linux/a
 chmod +x /usr/local/bin/kubeadm
 bash 6.k8s_install.sh
 if [ $? -eq 0 ]; then
-  kubectl taint  node  master node-role.kubernetes.io/control-plane:NoSchedule-
+  kubectl taint  node  master1 node-role.kubernetes.io/control-plane:NoSchedule-
 fi
 bash 9.addon_install.sh
