9f7f831bee8f49a4d1d911f801e1514435f0d592
long1318737396 <long1318737396@163.com>
2024-03-03 12:36:18 +0800

    update

--- offline/images/amd64/download.sh
+++ offline/images/amd64/download.sh
@@ -1,19 +1,19 @@
 set -x
 dir="$(cd "$(dirname "$0")" && pwd)"
 cd $dir
-source ../../conf/config.sh
+source ../../../conf/config.sh
 
 exec > >(tee -a "$logfile") 2>&1
 echo "$date_format"
 
 
-while read line;do nerdctl pull $line --insecure-registry;done < base-image.list
+while read line;do docker pull $line --insecure-registry;done < base-image.list
 
 
-while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); nerdctl save -o $result.tar.gz $line;done <base-image.list
+while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); docker save -o $result.tar.gz $line;done <base-image.list
 
 
-while read line;do nerdctl pull $line --insecure-registry;done < addon-image.list
+while read line;do docker pull $line --insecure-registry;done < addon-image.list
 
 
-while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); nerdctl save -o $result.tar.gz $line;done <addon-image.list
+while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); docker save -o $result.tar.gz $line;done <addon-image.list
--- offline/images/arm64/download.sh
+++ offline/images/arm64/download.sh
@@ -6,13 +6,13 @@ exec > >(tee -a "$logfile") 2>&1
 echo "$date_format"
 
 
-while read line;do nerdctl pull $line --insecure-registry --platform arm64;done < base-image.list
+while read line;do docker pull $line --insecure-registry --platform arm64;done < base-image.list
 
 
-while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); nerdctl save -o $result.tar.gz $line;done <base-image.list
+while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); docker save -o $result.tar.gz $line;done <base-image.list
 
 
-while read line;do nerdctl pull $line --insecure-registry --platform arm64;done < addon-image.list
+while read line;do docker pull $line --insecure-registry --platform arm64;done < addon-image.list
 
 
-while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); nerdctl save -o $result.tar.gz $line --platform arm64;done <addon-image.list
+while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); docker save -o $result.tar.gz $line --platform arm64;done <addon-image.list
