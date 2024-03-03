ea7f257e64abe3eaa5d5ea7e0506b16a2c91a8b6
long1318737396 <long1318737396@163.com>
2024-03-03 14:05:22 +0800

    devops_test

--- offline/images/amd64/download.sh
+++ offline/images/amd64/download.sh
@@ -7,13 +7,13 @@ exec > >(tee -a "$logfile") 2>&1
 echo "$date_format"
 
 
-while read line;do docker pull $line --insecure-registry;done < base-image.list
+while read line;do docker pull $line ;done < base-image.list
 
 
 while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); docker save -o $result.tar.gz $line;done <base-image.list
 
 
-while read line;do docker pull $line --insecure-registry;done < addon-image.list
+while read line;do docker pull $line ;done < addon-image.list
 
 
 while read line;do result=$(echo "$line" | awk -F'[/:]' '{ print $(NF-1) }'); docker save -o $result.tar.gz $line;done <addon-image.list
