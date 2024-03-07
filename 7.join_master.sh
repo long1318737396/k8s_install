1570449c07ea8ee5e99af4e701f533119c02dc64
long1318737396 <long1318737396@163.com>
2024-03-07 15:08:14 +0800

    update

--- 7.join_master.sh
+++ 7.join_master.sh
@@ -5,6 +5,10 @@ source conf/config.sh
 exec > >(tee -a "$logfile") 2>&1
 echo "$date_format"
 
-
+echo "$harbor_ip $harbor_hostname" >> /etc/hosts
 bash script/k8s/2.kubeadm-install.sh
+if [ $? -ne 0 ]; then
+  echo "k8s安装失败，请查看相关日志解决，然后执行kubeadm reset --force重置重新安装"
+  exit 1
+fi
 bash script/k8s/6.join-master.sh
