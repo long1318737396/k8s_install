241a91212edfa44b1bcba67ce8fd4611216731a2
long1318737396 <long1318737396@163.com>
2024-03-08 23:01:09 +0800

    update

---
+++ make-zh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+set -o pipefail
+bash offline/bin/amd64/download-binary-zh.sh
+
+bash offline/images/amd64/download-zh.sh
+
+bash offline/yum/amd64/centos8/build.sh
+
+mv _output/bin/* offline/bin/amd64/
+mv _output/images/* offline/images/amd64/
+
+
+cd ../
+tar --exclude='k8s_install/.git' -czvf k8s_install.tar.gz k8s_install/
