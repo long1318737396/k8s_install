25d65387879f6048724b63d33bec6cb5c5e7217d
long1318737396 <long1318737396@163.com>
2024-03-08 17:07:11 +0800

    update

--- offline/yum/amd64/centos8/build.sh
+++ offline/yum/amd64/centos8/build.sh
@@ -10,5 +10,9 @@ docker build -t registry.cn-hangzhou.aliyuncs.com/magictommy/centos8_yum:amd64 .
 docker run -d --name yum8 registry.cn-hangzhou.aliyuncs.com/magictommy/centos8_yum:amd64
 docker exec yum8 /bin/sh -c "cd  /usr/share/nginx/html;tar -czvf rpms.tar.gz rpms/"
 docker cp yum8:/usr/share/nginx/html/rpms.tar.gz .
-
+if [ $? -ne 0 ];then
+    echo "build yum8 failed"
+    exit 1
+fi
+docker rm -f yum8
 mv rpms.tar.gz ../
