2d54781d0fab573c52663f937faa4a83c64650d2
long1318737396 <long1318737396@163.com>
2024-03-13 16:52:08 +0800

    update loki

--- 1.yum_install_online.sh
+++ 1.yum_install_online.sh
@@ -1,24 +1,24 @@
 if [ -f /etc/debian_version ]; then
   apt update
    packages=(
-    wget* 
-    vim* 
-    conntrack* 
-    socat* 
-    ipvsadm* 
-    ipset* 
-    nmap* 
-    telnet* 
-    dnsutils*  
+    wget
+    vim 
+    conntrack
+    socat
+    ipvsadm 
+    ipset
+    nmap 
+    telnet 
+    dnsutils  
     nfs-kernel-server
     nfs-common
-    unzip*
+    unzip
     tar
-    bash-completion* 
-    tcpdump* 
-    mtr* 
-    nftables* 
-    iproute-tc*
+    bash-completion 
+    tcpdump
+    mtr
+    nftables 
+    iproute-tc
     iptables
     curl
     git
@@ -28,23 +28,23 @@ if [ -f /etc/debian_version ]; then
   done
 elif [ -f /etc/redhat-release ]; then
   packages=(
-    wget* 
-    vim* 
-    conntrack* 
-    socat* 
-    ipvsadm* 
-    ipset* 
-    nmap* 
-    telnet* 
-    bind-utils*  
-    nfs-utils* 
-    unzip*
+    wget
+    vim 
+    conntrack
+    socat
+    ipvsadm 
+    ipset
+    nmap 
+    telnet 
+    bind-utils  
+    nfs-utils 
+    unzip
     tar
-    bash-completion* 
-    tcpdump* 
-    mtr* 
-    nftables* 
-    iproute-tc*
+    bash-completion 
+    tcpdump
+    mtr
+    nftables 
+    iproute-tc
   )
 
   for i in ${packages[@]};do
@@ -52,23 +52,23 @@ elif [ -f /etc/redhat-release ]; then
   done
 else
     packages=(
-    wget* 
-    vim* 
-    conntrack* 
-    socat* 
-    ipvsadm* 
-    ipset* 
-    nmap* 
-    telnet* 
-    bind-utils*  
-    nfs-utils* 
-    unzip*
+    wget
+    vim 
+    conntrack 
+    socat
+    ipvsadm 
+    ipset
+    nmap 
+    telnet 
+    bind-utils  
+    nfs-utils 
+    unzip
     tar
-    bash-completion* 
-    tcpdump* 
-    mtr* 
-    nftables* 
-    iproute-tc*
+    bash-completion 
+    tcpdump
+    mtr
+    nftables 
+    iproute-tc
   )
 
   for i in ${packages[@]};do
